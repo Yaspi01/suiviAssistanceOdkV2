74d88fda8b13eb6aace814b2e750abcc86faa116
Yacouba Doumbia <yacoubaadoumbia@gmail.com>
2022-02-14 18:44:18 +0000

    message

--- src/main/java/com/odk/apisuiviapprenant/repositories/ressourceRepository/RessourceRepository.java
+++ src/main/java/com/odk/apisuiviapprenant/repositories/ressourceRepository/RessourceRepository.java
@@ -11,8 +11,7 @@ import java.util.List;
 
 public interface RessourceRepository extends JpaRepository<Ressource, Long> {
 
-    @Query(value = "SELECT res FROM Ressource res WHERE res.apprenant = :apprenant")
-    List<Ressource> ressourceApprenant(@Param("apprenant") Apprenant apprenant);
+
 
     @Query(value = "SELECT for FROM Ressource for WHERE for.formateur.id = :id")
     List<Ressource> ressourceByFormateur(@Param("id") Long id);

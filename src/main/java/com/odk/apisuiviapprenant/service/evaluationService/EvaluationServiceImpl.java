14fc3ef9ccee3e63fed01276af6607433252da27
Ibrahim Diawara <id2029937@gmail.com>
2022-04-05 11:41:19 +0000

    email sur la partie evaluation

--- src/main/java/com/odk/apisuiviapprenant/service/evaluationService/EvaluationServiceImpl.java
+++ src/main/java/com/odk/apisuiviapprenant/service/evaluationService/EvaluationServiceImpl.java
@@ -1,18 +1,24 @@
 package com.odk.apisuiviapprenant.service.evaluationService;
 
 import com.odk.apisuiviapprenant.enums.Niveau;
+import com.odk.apisuiviapprenant.exception.InvalidEntityException;
 import com.odk.apisuiviapprenant.models.evaluationModel.Evaluation;
 import com.odk.apisuiviapprenant.repositories.evaluationRepository.EvaluationRepository;
+import com.odk.apisuiviapprenant.service.MailSenderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
+import static com.odk.apisuiviapprenant.models.authers.Constante.URLFRONT;
 
 @Service
 public class EvaluationServiceImpl implements EvaluationService{
 
     @Autowired
     EvaluationRepository evaluationRepository;
+    @Autowired
+    MailSenderService senderService;
 
     @Override
     public Evaluation addEvaluation(Evaluation evaluation) {
@@ -56,9 +62,14 @@ public class EvaluationServiceImpl implements EvaluationService{
         if (evaluationFound.getNote() < 10){
             evaluationFound.setNiveau(Niveau.Faible);
         }
-        if (evaluationFound.getNote() > 20){
-           return null;
+        if (evaluationFound.getNote() > 20 || evaluationFound.getNote() < 0){
+            throw new InvalidEntityException("La note doit être comprise entre 0 et 20");
         }
+        senderService.sendSimpleEmail(
+                evaluation.getApprenant().getEmail(),"Bonjour "+evaluation.getApprenant().getPrenom()
+                        +" "+evaluation.getApprenant().getNom()+"\n"
+                +"Votre formateur a corrigé votre rendu sur le brief que vous venez de soumettre "+"\n"
+                +"Connectez vous à votre compte pour plus d'information "+ URLFRONT+"/user", "Correction de brief");
         return evaluationRepository.save(evaluationFound);
     }
 
